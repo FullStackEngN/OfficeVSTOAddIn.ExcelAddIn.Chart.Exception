cb13813664232c6a4c6bc485b0927b2a6ff359e0
FullStackEng <fullstackeng@outlook.com>
2018-12-14 16:56:53 +0800

    Initialize project

---
+++ ExcelAddIn.Chart.Exception/RibbonChartException.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ExcelAddIn.Chart.Exception
+{
+    partial class RibbonChartException : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public RibbonChartException()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.groupException = this.Factory.CreateRibbonGroup();
+            this.buttonAddChart = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.groupException.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.groupException);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // groupException
+            // 
+            this.groupException.Items.Add(this.buttonAddChart);
+            this.groupException.Label = "Chart Exception Sample";
+            this.groupException.Name = "groupException";
+            // 
+            // buttonAddChart
+            // 
+            this.buttonAddChart.Label = "Add Chart";
+            this.buttonAddChart.Name = "buttonAddChart";
+            this.buttonAddChart.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.buttonAddChart_Click);
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.RibbonChartException_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.groupException.ResumeLayout(false);
+            this.groupException.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup groupException;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton buttonAddChart;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal RibbonChartException Ribbon1
+        {
+            get { return this.GetRibbon<RibbonChartException>(); }
+        }
+    }
+}
---
+++ ExcelAddIn.Chart.Exception/RibbonChartException.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Tools.Excel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ExcelAddIn.Chart.Exception
+{
+    public partial class RibbonChartException
+    {
+        private void RibbonChartException_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void buttonAddChart_Click(object sender, RibbonControlEventArgs e)
+        {
+            Worksheet worksheet = Globals.Factory.GetVstoObject(
+                    Globals.ThisAddIn.Application.ActiveWorkbook.ActiveSheet);
+
+            worksheet.Range["A1", "A4"].Value2 = 1;
+            worksheet.Range["B1", "B4"].Value2 = 2;
+            worksheet.Range["C1", "C4"].Value2 = 3;
+            worksheet.Range["D1", "D4"].Value2 = 4;
+
+            // Create a first chart
+            double left = 10.00;
+            double top = 10.00;
+            double width = 200.00;
+            double height = 200.00;
+
+            Microsoft.Office.Tools.Excel.Chart chart = worksheet.Controls.AddChart(left, top, width, height, "test" + DateTime.Now.ToString("yyyymmddhhssfff"));
+            chart.ChartType = Excel.XlChartType.xl3DPie;
+
+            Excel.Range cells = worksheet.Range["A1", "D4"];
+            chart.SetSourceData(cells);
+
+            chart.PlotArea.Border.Color = ColorTranslator.ToOle(Color.Green);
+
+            try
+            {
+                chart.PlotArea.Left = 15.00;
+                chart.PlotArea.Top = 15.00;
+                chart.PlotArea.Width = 150.00;
+                chart.PlotArea.Height = 150.00;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
